dca7e3a2db4532f5c2d8db7d29c12a081c20fda5
KimDonggyeong <ehdrud0609@naver.com>
2023-05-18 23:27:47 +0900

    [Add] FairyWing E Skill Implement
    [Add] FairyWing E Skill Implement

---
+++ Source/SidheRigel/Character/FairyWing/Skill/FairyWingEProjectile.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FairyWingEProjectile.generated.h"
+
+UCLASS()
+class SIDHERIGEL_API AFairyWingEProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AFairyWingEProjectile();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+public:
+	UPROPERTY()
+		AActor* Target;
+	UPROPERTY()
+		AActor* projectileOwner;
+
+private:
+	UPROPERTY(EditAnywhere)
+		class USphereComponent* CollisionComponent;
+
+	UPROPERTY(EditAnywhere)
+		class UProjectileMovementComponent* ProjectileMovementComponent;
+
+	UPROPERTY(EditAnywhere)
+		class UStaticMeshComponent* ProjectileMesh;
+};
