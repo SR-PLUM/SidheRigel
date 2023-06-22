ff08fb3f7fd2cba79e8d69ba95fddad19ac674fd
KimDonggyeong <ehdrud0609@naver.com>
2023-06-22 19:48:55 +0900

    [Add] Implement BlackWizard R
    [Add] Implement BlackWizard R

--- Source/SidheRigel/Character/BlackWizard/BlackWizardCharacter.cpp
+++ Source/SidheRigel/Character/BlackWizard/BlackWizardCharacter.cpp
@@ -2,12 +2,21 @@
 
 
 #include "BlackWizardCharacter.h"
+#include "Components/SphereComponent.h"
+#include "SidheRigel/Character/BlackWizard/Skill/BlackWizardRCollider.h"
+//#include "SidheRigel/Character/BlackWizard/BlackWizardAttackProjectile.h"
 
 // Sets default values
 ABlackWizardCharacter::ABlackWizardCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	static ConstructorHelpers::FObjectFinder<UBlueprint> RCollider(TEXT("/Game/Heros/BlackWizard/Skill/BP_BlackWizardRCollider"));
+	if (RCollider.Object)
+	{
+		RColliderClass = (UClass*)RCollider.Object->GeneratedClass;
+	}
 
 }
 
@@ -32,3 +41,106 @@ void ABlackWizardCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInp
 
 }
 
+void ABlackWizardCharacter::SkillOne()
+{
+	UE_LOG(LogTemp, Warning, TEXT("BlackWizard Ready Q"));
+
+	skillState = Q_Ready;
+}
+
+void ABlackWizardCharacter::SkillTwo()
+{
+	UE_LOG(LogTemp, Warning, TEXT("BlackWizard Ready W"));
+
+	skillState = W_Ready;
+}
+
+void ABlackWizardCharacter::SkillThree()
+{
+	UE_LOG(LogTemp, Warning, TEXT("BlackWizard Ready E"));
+
+	skillState = E_Ready;
+}
+
+void ABlackWizardCharacter::SkillFour()
+{
+	UE_LOG(LogTemp, Warning, TEXT("BlackWizard Ready R"));
+
+	skillState = R_Ready;
+}
+
+void ABlackWizardCharacter::SkillCancel()
+{
+	UE_LOG(LogTemp, Warning, TEXT("BlackWizard SkillCancel"));
+
+	skillState = Null;
+}
+
+void ABlackWizardCharacter::UseSkill(FHitResult HitResult)
+{
+	switch (skillState)
+	{
+	case Null:
+		UE_LOG(LogTemp, Warning, TEXT("BlackWizard SkillState is Null"));
+		break;
+	case Q_Ready:
+		UE_LOG(LogTemp, Warning, TEXT("BlackWizard use Q"));
+		skillState = Null;
+		break;
+	case W_Ready:
+		UE_LOG(LogTemp, Warning, TEXT("BlackWizard use W"));
+		skillState = Null;
+		break;
+	case E_Ready:
+		UE_LOG(LogTemp, Warning, TEXT("BlackWizard use E"));
+		skillState = Null;
+		break;
+	case R_Ready:
+		UE_LOG(LogTemp, Warning, TEXT("BlackWizard use R"));
+
+		if (RColliderClass)
+		{
+			FVector PawnToTarget = (HitResult.Location - GetActorLocation()).GetSafeNormal();
+			PawnToTarget *= FVector(1, 1, 0);
+			FVector MuzzleLocation = (GetActorLocation() + PawnToTarget);
+			FRotator MuzzleRotation = PawnToTarget.Rotation();
+			UWorld* World = GetWorld();
+			if (World)
+			{
+				FActorSpawnParameters SpawnParams;
+				FTransform SpawnTransform;
+				SpawnTransform.SetLocation(MuzzleLocation);
+				SpawnTransform.SetRotation(MuzzleRotation.Quaternion());
+				SpawnParams.Owner = this;
+				SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+				//SpawnParams.Instigator = GetInstigator();
+
+				// Spawn the projectile at the muzzle.
+				ABlackWizardRCollider* Collider = World->SpawnActorDeferred<ABlackWizardRCollider>(RColliderClass, SpawnTransform);
+				if (Collider)
+				{
+					// Set the projectile's initial trajectory.
+					Collider->colliderOwner = this;
+
+					FTimerHandle RColliderDestroyTimer;
+					GetWorldTimerManager().SetTimer(RColliderDestroyTimer,
+						FTimerDelegate::CreateLambda([=]()
+							{
+								Collider->Destroy();
+							}
+
+					), 1.0f, false);					
+
+					Collider->FinishSpawning(SpawnTransform);
+
+					Collider->CollisionComponent->SetGenerateOverlapEvents(false);
+				}
+			}
+		}
+
+		skillState = Null;
+		break;
+	default:
+		break;
+	}
+}
--- Source/SidheRigel/Character/BlackWizard/BlackWizardCharacter.h
+++ Source/SidheRigel/Character/BlackWizard/BlackWizardCharacter.h
@@ -1,10 +1,9 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "SidheRigel/SidheRigelCharacter.h"
+#include "SidheRigel/Enum/E_SkillState.h"
 #include "BlackWizardCharacter.generated.h"
 
 UCLASS()
@@ -19,12 +18,24 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+protected:	
+	virtual void SkillOne();
+	virtual void SkillTwo();
+	virtual	void SkillThree();
+	virtual void SkillFour();
+
+	virtual void SkillCancel();
+	virtual void UseSkill(FHitResult HitResult);
+
+private:
+	E_SkillState skillState;
+	UPROPERTY()
+		FTimerHandle destroyTimer;
+
+protected:	//Skill Projectile
+	UPROPERTY(EditDefaultsOnly)
+		TSubclassOf<class ABlackWizardRCollider> RColliderClass;
 };
