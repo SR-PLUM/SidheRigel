defd7ebaca8317cb9e9ffc82fa568dfe2d9d1244
yoon-H <ucando0320@gmail.com>
2023-08-27 17:17:30 +0900

    [Update] ACMQCollider
    [Update] ACMQCollider

--- Source/SidheRigel/Character/ACM/Skills/ACMQCollider.h
+++ Source/SidheRigel/Character/ACM/Skills/ACMQCollider.h
@@ -23,8 +23,49 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-protected:
+public:	
+
 	UPROPERTY()
+	class AActor* ColliderOwner;
+
+	UPROPERTY()
+		bool IsUpgraded;
+
+	UPROPERTY(VisibleAnywhere)
 	class UStaticMeshComponent* MeshComponent;
+
+protected:
+
+	UPROPERTY()
+	float CurrentDamage;
+
+	UPROPERTY()
+	float Damage = 10.f;
+
+	UPROPERTY()
+	float UpgradedDamage = 15.f;
+
+	UPROPERTY()
+	float CurrentRange;
+
+	UPROPERTY()
+	float Range = 1.f;
+
+	UPROPERTY()
+	float UpgradedRange = 2.f;
+
+	UPROPERTY()
+	float DecreasingDefensePointAmount;
+
+	UPROPERTY()
+	float DecreasingDefensePointTime;
+
+protected:
+	UFUNCTION()
+		void OnColliderOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+public:
+	UFUNCTION()
+	void InitStatus();
 
 };
