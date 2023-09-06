cb21198baf99fd0582ab96dbcfe138711fd7ce03
yoon-H <ucando0320@gmail.com>
2023-09-06 16:57:14 +0900

    [Add] add Widget cpp files, ItemSlot binding
    [Add] add Widget cpp files, ItemSlot binding

---
+++ Source/SidheRigel/UI/CharacterStatus.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CharacterStatus.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SIDHERIGEL_API UCharacterStatus : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
