6f3ecc903d23326c2fa972cce5363a445b956c2d
yoon-H <ucando0320@gmail.com>
2023-09-09 17:02:55 +0900

    [Add] bind CharacterStatus Widget
    [Add] bind CharacterStatus Widget

--- Source/SidheRigel/UI/CharacterStatus.h
+++ Source/SidheRigel/UI/CharacterStatus.h
@@ -13,5 +13,77 @@ UCLASS()
 class SIDHERIGEL_API UCharacterStatus : public UUserWidget
 {
 	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(meta = (BindWidget))
+		class UImage* CharacterImage;
+
+	UPROPERTY(meta = (BindWidget))
+		class UTextBlock* CurrentLevelText;
+
+	UPROPERTY(meta = (BindWidget))
+		class UProgressBar* LevelProgress;
+
+	//Skill Button
+	UPROPERTY(meta = (BindWidget))
+		class UButton* Btn_QSkill;
+
+	UPROPERTY(meta = (BindWidget))
+		class UButton* Btn_WSkill;
+
+	UPROPERTY(meta = (BindWidget))
+		class UButton* Btn_ESkill;
+
+	UPROPERTY(meta = (BindWidget))
+		class UButton* Btn_RSkill;
+
+	UPROPERTY(meta = (BindWidget))
+		class UButton* Btn_BSkill;
+
+	//HP, MP
+	UPROPERTY(meta = (BindWidget))
+		class UProgressBar* HPBar;
+
+	UPROPERTY(meta = (BindWidget))
+		class UTextBlock* CurrentHPText;
+
+	UPROPERTY(meta = (BindWidget))
+		class UTextBlock* MaxHpText;
+
+	UPROPERTY(meta = (BindWidget))
+		class UProgressBar* MPBar;
+
+	UPROPERTY(meta = (BindWidget))
+		class UTextBlock* CurrentMPText;
+
+	UPROPERTY(meta = (BindWidget))
+		class UTextBlock* MaxMpText;
+
+	//ItemSlot
+	UPROPERTY(meta = (BindWidget))
+		class UItemSlot* ItemSlot1;
+
+	UPROPERTY(meta = (BindWidget))
+		class UItemSlot* ItemSlot2;
+
+	UPROPERTY(meta = (BindWidget))
+		class UItemSlot* ItemSlot3;
+
+	UPROPERTY(meta = (BindWidget))
+		class UItemSlot* ItemSlot4;
+
+	UPROPERTY(meta = (BindWidget))
+		class UItemSlot* ItemSlot5;
+
+	UPROPERTY(meta = (BindWidget))
+		class UItemSlot* ItemSlot6;
+
+	//Shop
+	UPROPERTY(meta = (BindWidget))
+		class UButton* Btn_Shop;
+
+	UPROPERTY(meta = (BindWidget))
+		class UTextBlock* MoneyText;
 	
 };
