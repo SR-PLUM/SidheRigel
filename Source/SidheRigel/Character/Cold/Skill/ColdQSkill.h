f72bb7dead36084546a5000d5d161e3f6f1d26d8
Seobok <Seobok@users.noreply.github.com>
2023-11-05 17:17:12 +0900

    [Update] Cold E Skill Animation, Cold Q Skill Speed, Scale
    [Update] Cold E Skill Animation, Cold Q Skill Speed, Scale

--- Source/SidheRigel/Character/Cold/Skill/ColdQSkill.h
+++ Source/SidheRigel/Character/Cold/Skill/ColdQSkill.h
@@ -21,7 +21,7 @@ public:
 	virtual void OnUse(FHitResult Hit) override;
 
 private:	//Property
-	float colliderSpeed = 500.f;
+	float colliderSpeed = 1000.f;
 	float colliderDelay = 0.1f;
 	float colliderDamage = 20.f;
 	int32 count = 10;
