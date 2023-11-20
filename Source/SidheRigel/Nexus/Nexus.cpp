072f267c2da6826a1942d56d14cdfafd9bf0c07f
KimDonggyeong <ehdrud0609@naver.com>
2023-11-21 01:13:43 +0900

    [Update] Nexus can Attack
    [Update] Nexus can Attack

---
+++ Source/SidheRigel/Nexus/Nexus.cpp
@@ -0,0 +1,198 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Nexus.h"
+
+#include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/WidgetComponent.h"
+
+#include "SidheRigel/UI/HPUI.h"
+#include "SidheRigel/Nexus/NexusAttackProjectile.h"
+
+// Sets default values
+ANexus::ANexus()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	if (!RootComponent)
+	{
+		RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("SceneComponent"));
+	}
+
+	if (!mesh)
+	{
+		mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+		mesh->SetupAttachment(rangeArea);
+
+		static ConstructorHelpers::FObjectFinder<UStaticMesh>meshRef(TEXT("/Game/Nexus/SM_Nexus"));
+		if (meshRef.Succeeded())
+		{
+			mesh->SetStaticMesh(meshRef.Object);
+		}
+		RootComponent = mesh;
+	}
+
+	if (!rangeArea)
+	{
+		rangeArea = CreateDefaultSubobject<USphereComponent>(TEXT("rangeArea"));
+		rangeArea->InitSphereRadius(30.0f);
+		rangeArea->SetupAttachment(mesh);
+	}
+
+	static ConstructorHelpers::FObjectFinder<UBlueprint> projectileRef(TEXT("/Game/Nexus/BP_NexusAttackProjectile"));
+	if (projectileRef.Object)
+	{
+		projectileClass = (UClass*)projectileRef.Object->GeneratedClass;
+	}
+
+	if (!muzzleLocation)
+	{
+		muzzleLocation = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MuzzleLocation"));
+		muzzleLocation->SetupAttachment(mesh);
+	}
+
+	HP = MaxHP;
+}
+
+void ANexus::BeginPlay()
+{
+	Super::BeginPlay();
+
+	rangeArea->OnComponentBeginOverlap.AddDynamic(this, &ANexus::OnEnterEnemy);
+	rangeArea->OnComponentEndOverlap.AddDynamic(this, &ANexus::OnExitEnemy);
+}
+
+// Called when the game starts or when spawned
+
+
+// Called every frame
+void ANexus::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (attackDelay > 0)
+	{
+		attackDelay -= DeltaTime;
+	}
+
+	if (currentTarget)
+	{
+		IDamagable* damagableTarget = Cast<IDamagable>(currentTarget);
+		if (damagableTarget)
+		{
+			if (damagableTarget->GetHP() <= 0)
+			{
+				attackList.Remove(currentTarget);
+
+				if (attackList.Num() == 0)
+				{
+					currentTarget = nullptr;
+				}
+				else
+				{
+					currentTarget = attackList.Top();
+				}
+			}
+			else if (attackDelay <= 0)
+			{
+				FVector MuzzleLocation = muzzleLocation->GetComponentLocation();
+				FRotator MuzzleRotation = GetActorRotation();
+
+				UWorld* World = GetWorld();
+				if (World)
+				{
+					FActorSpawnParameters SpawnParams;
+					FTransform SpawnTransform;
+					SpawnTransform.SetLocation(MuzzleLocation);
+					SpawnTransform.SetRotation(MuzzleRotation.Quaternion());
+					SpawnParams.Owner = this;
+					SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+					ANexusAttackProjectile* projectile = World->SpawnActorDeferred<ANexusAttackProjectile>(projectileClass, SpawnTransform);
+					if (projectile)
+					{
+						projectile->Target = currentTarget;
+						projectile->AttackDamage = damage;
+						projectile->criticalDamage = 1;
+						projectile->criticalRate = 0;
+
+						projectile->projectileOwner = this;
+					}
+					projectile->FinishSpawning(SpawnTransform);
+				}
+
+				attackDelay = maxAttackDelay;
+			}
+		}
+	}
+}
+
+void ANexus::OnEnterEnemy(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (ITeam* Enemy = Cast<ITeam>(OtherActor))
+	{
+		if (Enemy->GetTeam() != GetTeam())
+		{
+			if (IDamagable* DamagableActor = Cast<IDamagable>(OtherActor))
+			{
+				if (attackList.Num() == 0)
+				{
+					currentTarget = OtherActor;
+				}
+
+				attackList.Add(OtherActor);
+			}
+		}
+	}
+}
+
+void ANexus::OnExitEnemy(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if (ITeam* Enemy = Cast<ITeam>(OtherActor))
+	{
+		if (IDamagable* DamagableActor = Cast<IDamagable>(OtherActor))
+		{
+			attackList.Remove(OtherActor);
+
+			if (currentTarget == OtherActor)
+			{
+				if (attackList.IsEmpty())
+				{
+					currentTarget = nullptr;
+				}
+				else
+				{
+					currentTarget = attackList.Top();
+				}
+			}
+		}
+	}
+}
+
+void ANexus::TakeDamage(float _damage, AActor* damageCauser)
+{
+	HP -= _damage;
+
+	if (HP <= 0)
+	{
+		//Destroy Animation
+		Destroy();
+	}
+}
+
+void ANexus::RestoreHP(float value)
+{
+}
+
+float ANexus::GetHP()
+{
+	return HP;
+}
+
+E_Team ANexus::GetTeam()
+{
+	return team;
+}
+
---
+++ Source/SidheRigel/Nexus/Nexus.h
@@ -0,0 +1,85 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+
+#include "../Interface/Damagable.h"
+#include "../Interface/Team.h"
+
+#include "Nexus.generated.h"
+
+UCLASS()
+class SIDHERIGEL_API ANexus : public AActor, public IDamagable, public ITeam
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ANexus();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+protected:
+	class USphereComponent* rangeArea;
+
+	UPROPERTY(VisibleAnywhere)
+		class UStaticMeshComponent* mesh;
+
+	UPROPERTY(EditAnywhere)
+		TEnumAsByte<E_Team> team = E_Team::Team_Null;
+
+	float HP;
+	float MaxHP = 3000;
+
+//protected:		UI
+//	UPROPERTY()
+//		class UWidgetComponent* TowerWidget;
+//	UPROPERTY()
+//		class UHPUI* TowerUIRef;
+//
+//	void InitTowerWidget();
+//	void InitTowerUI();
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION()
+		void OnEnterEnemy(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	UFUNCTION()
+		void OnExitEnemy(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+public:
+	TSubclassOf<class ANexusAttackProjectile> projectileClass;
+
+	UPROPERTY(EditAnywhere)
+		class UStaticMeshComponent* muzzleLocation;
+
+	UPROPERTY()
+		TArray<class AActor*> attackList;
+
+	UPROPERTY()
+		AActor* currentTarget;
+
+	UPROPERTY()
+		float attackDelay = 0.f;
+	UPROPERTY()
+		float maxAttackDelay = 1.5f;
+
+	UPROPERTY()
+		float damage = 150.f;
+public:
+	UFUNCTION()
+		virtual void TakeDamage(float _damage, AActor* damageCauser);
+	UFUNCTION()
+		virtual void RestoreHP(float value);
+	UFUNCTION()
+		virtual float GetHP();
+
+	UFUNCTION()
+		virtual E_Team GetTeam();
+};
