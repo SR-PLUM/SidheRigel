27d03b4e3feee311d840287de1ccab5b933ad217
yoon-H <ucando0320@gmail.com>
2023-11-21 15:18:16 +0900

    [Add] add SidheRigelCamera
    [Add] add SidheRigelCamera

---
+++ Source/SidheRigel/SidheRigelCamera.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SidheRigelCamera.h"
+
+// Sets default values
+ASidheRigelCamera::ASidheRigelCamera()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASidheRigelCamera::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASidheRigelCamera::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
